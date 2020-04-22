37e89afdf8c865a2bf7baa55a683b3e786161c6e
Dustin Firebaugh <dafirebaugh@gmail.com>
2020-04-21 23:38:24 -0400

    file output is now putting out an array of bytes

--- src/file.c
+++ src/file.c
@@ -1,16 +1,24 @@
 #include "file.h"
 
-static void print_sprite(FILE *output, const char sprite_index)
+static void print_sprite_as_hex(FILE *output, const char sprite_index)
 {
     char i;
     for (i = 0; i < SPRITE_CANVAS_SIZE; i++)
     {
-        if (i % 2 == 0)
-            printf(" ");
+        if (i == 0)
+        {
+            fprintf(output,"{ 0x");
+        }
+        else if (i % 2 == 0)
+        {
+            fprintf(output,", 0x");
+        }
 
-        if (i % 8 == 0)
-            fprintf(output, "\n");
         fprintf(output, "%x", sprite_sheet_cells[sprite_index].pixels[i]);
+
+        if (i == SPRITE_CANVAS_SIZE-1)
+            fprintf(output, " }\n");
+
     }
 
     fprintf(output, "\n");
@@ -20,14 +28,14 @@ static void print_sprite_sheet(FILE *output)
 {
     char i;
     for (i = 0; i < SPRITESHEET_CANVAS_SIZE; i++)
-        print_sprite(output, i);
+        print_sprite_as_hex(output, i);
 }
 
 extern void save_file()
 {
     FILE *fp;
 
-    fp = fopen("spritesheet.txt", "w+");
+    fp = fopen("_spritesheet.txt", "w+");
     print_sprite_sheet(fp);
     fclose(fp);
 }
